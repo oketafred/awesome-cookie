7251a23e1f81945778e3e3f9afdcb410e9b9861a
Oketa Fred <oketafred@gmail.com>
2022-07-16 17:40:04 +0300

    Adds setCookie, getCookie and removieCookie functions

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# awesome-cookie
+# Awesome Cookie
+
+This is a simple Javascript API for makes working and manipulating cookies a breeze.
+
+## Installation
+```
+npm install awesome-cookie
+```
+## Usage
+
+```javascript
+import { setCookie, getCookie, removeCookie } from 'awesome-cookie';
+
+setCookie('name', 'value');
+
+getCookie('name');
+
+removeCookie('name');
+```
+
---
+++ index.js
@@ -0,0 +1,38 @@
+export function getCookie(name) {
+    const cookieName = `${encodeURIComponent(name)}=`;
+    const cookie = document.cookie;
+    let value = null;
+
+    const startIndex = cookie.indexOf(cookieName);
+    if (startIndex > -1) {
+        let endIndex = cookie.indexOf(';', startIndex);
+        if (endIndex === -1) {
+            endIndex = cookie.length;
+        }
+        value = decodeURIComponent(
+            cookie.substring(startIndex + name.length, endIndex)
+        );
+    }
+
+    if (value) {
+        return value.substring(1);
+    }
+    return value;
+}
+
+export function setCookie(name, value, expires, path, domain, secure) {
+    let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+    if (expires instanceof Date) {
+        cookieText += `; expires=${expires.toGMTString()}`;
+    }
+
+    if (path) cookieText += `; path=${path}`;
+    if (domain) cookieText += `; domain=${domain}`;
+    if (secure) cookieText += `; secure`;
+
+    document.cookie = cookieText;
+}
+
+export function removeCookie(name, path, domain, secure) {
+    setCookie(name, '', new Date(0), path, domain, secure);
+}
